8ea51c9f21782c872c11ce0e65de8e91625a4a53
Arvid <arvid.alund@gmail.com>
2025-10-31 18:30:50 +0100

    feature: added icons with tooltip and contact function

---
+++ src/app/components/iconWTooltip.jsx
@@ -0,0 +1,24 @@
+export default function IconWTooltip({ name, href, svg }) {
+  return (
+    <a
+      href={href}
+      target="_blank"
+      rel="noopener noreferrer"
+      className="group relative inline-block w-6 h-6 text-white hover:text-neutral-400 transition-colors"
+    >
+      {/* SVG */}
+      <div className="h-5 fill-white">{svg}</div>
+
+      {/* Tooltip */}
+      <span className="
+        absolute bottom-1/2 left-1/2 -translate-x-1/2 mb-2
+        px-2 py-1 text-xs bg-neutral-900 text-white rounded
+        opacity-0 pointer-events-none
+        transition-all duration-200
+        group-hover:opacity-100 group-hover:bottom-full
+      ">
+        {name}
+      </span>
+    </a>
+  );
+}
