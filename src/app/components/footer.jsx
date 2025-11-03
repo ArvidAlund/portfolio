ef1b87ad9bf2f43406015b480874ac3790909eec
Arvid <arvid.alund@gmail.com>
2025-11-03 19:28:28 +0100

    fix: footer alignment

--- src/app/components/footer.jsx
+++ src/app/components/footer.jsx
@@ -2,7 +2,7 @@ import ContactIcons from "./contact"
 export default function Footer(){
     return <footer className="mx-auto max-w-3xl px-8 py-6 flex justify-between items-center">
         <div className="flex items-center text-sm">
-            <div className="flex gap-2 text-neutral-400 p-1 border-r">
+            <div className="flex gap-2 text-neutral-400 p-1 border-r items-center justify-center">
                 <p>© 2025</p>
                 <a href="https://arvidalund.com" className="hover:underline">arvidalund.com</a>
             </div>
